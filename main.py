a54b7b31293a4c057554109de81f835ee9f913a7
hardik7863 <hardikbatwal@gmail.com>
2025-02-08 19:05:10 +0530

    Model Evaluation

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
-from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline 
+from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
+from src.datascience.pipeline.model_evaluation_pipeline import ModelEvaluationTrainingPipeline
+
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -12,6 +14,7 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
 
 STAGE_NAME = "Data Validation stage"
 try:
@@ -34,6 +37,7 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
 STAGE_NAME = "Model Trainer stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
@@ -43,3 +47,18 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+STAGE_NAME = "Model evaluation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelEvaluationTrainingPipeline()
+   data_ingestion.initiate_model_evaluation()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+
