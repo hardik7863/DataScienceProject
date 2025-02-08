52c3d7b75fca76abbb2d1e83f9a4abec07cd6738
hardik7863 <hardikbatwal@gmail.com>
2025-02-08 11:01:47 +0530

    data Ingestion

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from src.datascience import logger
-logger.info("welcome to our custom logging data science")
+from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.initiate_data_ingestion()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
